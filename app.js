45f4180ebde74e38a75067c2f9e4806679b8d6a4
jason-ono <exia1999@gmail.com>
2021-06-17 16:49:15 -0400

    POST method added

--- app.js
+++ app.js
@@ -9,9 +9,18 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res) {
   // the index.html in our directry
   res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var email = req.body.email;
+  console.log(firstName, lastName, email);
 });
 
 app.listen(3000, function(){
